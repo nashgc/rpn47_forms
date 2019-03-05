6e6377655cb6d5eeb5a9da0b3117625047c7685d
KA <nashgc@gmail.com>
2019-03-05 19:56:20 +0300

    static js, css were added

---
+++ rpn47_forms/static/js/script.js
@@ -0,0 +1,91 @@
+// INN AJAX request
+
+$(document).ready(function () {
+    $("#inn").autocomplete({
+        minLength: 3,
+        source: function (request, response) {
+            var form = $(this);
+            $.ajax({
+                url: url,
+                processData: true,
+                type: 'POST',
+
+                data: {
+                    search: request.term,
+                    csrfmiddlewaretoken: csrf_token,
+                },
+                success: function (data) {
+                    // console.log(data)
+                    response(data);
+                },
+
+            });
+        },
+        select: function (event, ui) {
+            // console.log(ui.item)
+            $('.org').val(ui.item.org)
+        }
+    });
+
+// rasp_num generator
+
+    var dash = '-'
+    var slash = '/'
+    var reg = '78'
+    var district_code = '__'
+    var department_code = '__'
+    var doc_type = '19'
+    var adm_num_calc = parseInt(gdn)
+    var adm_number = ("0000" + adm_num_calc).slice(-4)
+    var year = (new Date()).getFullYear()
+
+
+    $("#district").on('change', function () {
+        district_code = this.value.substring(0,2)
+        $("#raspr_num").val(reg+dash+district_code+dash+department_code+slash+doc_type+dash+adm_number+dash+year)
+    })
+
+
+    $("#department").on('change', function () {
+        department_code = this.value.substring(0,2)
+        $("#raspr_num").val(reg+dash+district_code+dash+department_code+slash+doc_type+dash+adm_number+dash+year)
+    })
+
+
+    $("#raspr_num").val(reg+dash+district_code+dash+department_code+slash+doc_type+dash+adm_number+dash+year)
+
+
+    // get and set current date
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+    dd = '0'+dd
+    }
+
+    if(mm<10) {
+        mm = '0'+mm
+    }
+
+    // bylaw
+    $("#raspr_date, #date_proved, #date_proved_po, #date_proved_c").val(yyyy + '-' + mm + '-' + dd)
+    
+    //ordinance
+    $("#ordinance_date, #pay_date, #fact_pay_date, #income_receipt_date, #protocol_date").val(yyyy + '-' + mm + '-' + dd)
+
+    // datePicker loads here
+
+    // bylaw
+    $("#raspr_date, #date_proved_po, #date_proved_c, #date_proved").datepicker({
+    todayButton: new Date()
+    })
+
+    //ordinance
+    $("#ordinance_date, #pay_date, #fact_pay_date, #income_receipt_date, #protocol_date").datepicker({
+        todayButton: new Date()
+    })
+
+});
+
