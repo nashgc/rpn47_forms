10f44e2c1ce83a91343a0b6e6b04076cc7702151
KA <nashgc@gmail.com>
2019-03-21 15:49:13 +0300

    - remove pagination - add all ordinance fields to search

--- search/views.py
+++ search/views.py
@@ -21,23 +21,6 @@ def search(request):
         else:
             html = render_to_string('search/search.html', {'raspr': iins})
         return HttpResponse(html)
-    elif request.method == 'GET':
-        search_text = ''
-        bylaw_iins = BylawModel.objects.all().filter(inn__icontains=search_text)
-        bylaw_iins.model.model_name = 'Распоряжение'
-        ordinance_iins = OrdinanceModel.objects.all().filter(inn__icontains=search_text)
-        ordinance_iins.model.model_name = 'Постановление'
-        iins = list(chain(bylaw_iins, ordinance_iins))
-        try:
-            page = request.GET.get('page', 1)
-        except PageNotAnInteger:
-            page = 1
-        paginator = Paginator(iins, 20, request=request)
-        raspr = paginator.page(page)
-        if not iins:
-            return render(request, 'search/search.html', {'not_found': "ИНН {} не найдено".format(search_text)})
-        else:
-            return render(request, 'search/search.html', {'raspr': raspr})
     else:
         search_text = request.POST['search']
         bylaw_iins = BylawModel.objects.all().filter(inn__icontains=search_text)
@@ -45,14 +28,8 @@ def search(request):
         ordinance_iins = OrdinanceModel.objects.all().filter(inn__icontains=search_text)
         ordinance_iins.model.model_name = 'Постановление'
         iins = list(chain(bylaw_iins, ordinance_iins))
-        try:
-            page = request.GET.get('page', 1)
-        except PageNotAnInteger:
-            page = 1
-        paginator = Paginator(iins, 20, request=request)
-        raspr = paginator.page(page)
-        print(raspr)
+        print(iins)
         if not iins:
             return render(request, 'search/search.html', {'not_found': "ИНН {} не найдено".format(search_text)})
         else:
-            return render(request, 'search/search.html', {'raspr': raspr})
+            return render(request, 'search/search.html', {'raspr': iins})
